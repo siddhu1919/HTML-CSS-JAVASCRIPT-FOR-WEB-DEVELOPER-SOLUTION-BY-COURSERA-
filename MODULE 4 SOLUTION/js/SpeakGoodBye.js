0fe9bbd0636c661bacc0b7089c56d6d690761915
SIDDHARTH GAUTAM SINGH <85176765+siddhu1919@users.noreply.github.com>
2022-07-09 19:42:52 +0530

    Create SpeakGoodBye.js

---
+++ MODULE 4 SOLUTION/js/SpeakGoodBye.js
@@ -0,0 +1,19 @@
+(function (window){
+
+    var byeSpeaker = {};
+
+    var speakWord = "Good Bye";
+
+    
+
+    byeSpeaker.speak = function (names) {
+
+      console.log(speakWord + " " + names);
+
+    }
+
+     
+
+     window.byeSpeaker = byeSpeaker;
+
+    })(window);
